69e42e609ee799fb07c860e71045248d073c0fff
yu013012 <youdanyu943@gmail.com>
2023-09-14 14:39:13 +0900

    追加修正

--- contexts/MyContext.tsx
+++ contexts/MyContext.tsx
@@ -4,7 +4,6 @@ export const MyContext = createContext('');
 
 export function useMyContext() {
   const context = useContext(MyContext);
-  console.log(context)
   if (context === undefined) {
     throw new Error('useMyContext must be used within a MyContextProvider');
   }
